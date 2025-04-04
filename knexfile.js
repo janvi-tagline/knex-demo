5d75d7b8a34251a586500554c13e6b64af2eee00
janvi-tagline <janvid.tagline@gmail.com>
2025-04-04 10:53:24 +0530

    Add comments

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+//this is the knexfile.js file , it is used to configure the database connection for the project,
+//It is used to create the migrations and seeds for the project
+//You can use this inside the config folder also. but now every time you need to specify the path of the file. while creating the migrations and seeds.
+//so we are using this file in the root directory.
+//if you didn't specify the path of the file. while creating the migrations and seeds. it will create the migrations and seeds in the root directory.instead of specific location.
 
 module.exports = {
   development: {
